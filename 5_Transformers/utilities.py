1df5370cd207c7df1c025a14240a5669f08d561e
PranavC225 <pranav.chandode@gmail.com>
2025-05-02 17:35:21 +0200

    adding the mlp code

--- 5_Transformers/utilities.py
+++ 5_Transformers/utilities.py
@@ -188,8 +188,8 @@ def mlp(x, hidden_units, dropout_rate):
     # --------------------------------------------
 
     for units in hidden_units:
-        ...
-        ...
+        x = tf.keras.layers.Dense(units, activation='relu')(x)
+        x = tf.keras.layers.Dropout(dropout_rate)(x)
 
     # ============================================
     return x
